5915239867584169be9fe1f280802752363720c0
wforguo <wforguo@qq.com>
2023-09-24 21:53:35 +0800

    feat: remove readdirSync

--- src/api/v1/weapp/index.ts
+++ src/api/v1/weapp/index.ts
@@ -1,20 +1,26 @@
 import Router from '@koa/router'
 import fs from 'fs'
+import jssdk from './jssdk'
 
 const route = new Router({
     prefix: '/weapp'
 })
 
 // 读取并遍历所有路由文件
-fs.readdirSync(__dirname).forEach((file: string) => {
-    // 过滤index.ts文件
-    if (file !== 'index.ts') {
-        // 引入路由文件
-        const $route = require(`./${file}`)
-        // 注册路由
-        // allowedMethods 响应options请求
-        route.use($route.default.routes()).use($route.default.allowedMethods())
-    }
-})
+// try {
+//     fs.readdirSync(__dirname).forEach((file: string) => {
+//         console.log(file)
+//         // 过滤index.ts文件
+//         if (file !== 'index.ts') {
+//             // 引入路由文件
+//             // 注册路由
+//             // allowedMethods 响应options请求
+//         }
+//     })
+// } catch (e) {
+//     throw e
+// }
+
+route.use(jssdk.routes()).use(jssdk.allowedMethods())
 
 export default route
